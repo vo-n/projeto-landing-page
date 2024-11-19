fe29313f1c622931f3132b2f2ce3a0afb93fb8ca
vo-n <guilhermehigaf@gmail.com>
2024-11-19 16:46:26 -0300

    iniciando o projeto

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const uglify = require('gulp-uglify');
+
+function scripts() {
+  return gulp.src('./src/scripts/*.js')
+  .pipe(uglify())
+  .pipe(gulp.dest('./dist/js'));
+}
+
+function styles() {
+  return gulp.src('./src/styles/*.scss')
+  .pipe(sass({ outputStyle: 'compressed' }))
+  .pipe(gulp.dest('./dist/css'));
+}
+
+exports.default = gulp.parallel(styles, scripts);
+exports.watch = function() {
+  gulp.watch('./src/styles/*.scss', gulp.parallel(styles));
+  gulp.watch('./src/scripts/*.js', gulp.parallel(scripts));
+}
