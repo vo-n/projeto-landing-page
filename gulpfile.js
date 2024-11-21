8e28489b7c600fa0af55c6cb33d86da2e15a5c6b
vo-n <guilhermehigaf@gmail.com>
2024-11-21 15:01:46 -0300

    adicionando a estrutura das sections

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const uglify = require('gulp-uglify');
+const imagemin = require('gulp-imagemin');
 
 function scripts() {
   return gulp.src('./src/scripts/*.js')
@@ -14,7 +15,13 @@ function styles() {
   .pipe(gulp.dest('./dist/css'));
 }
 
-exports.default = gulp.parallel(styles, scripts);
+function images() {
+  return gulp.src('./src/images/**/*')
+  .pipe(imagemin())
+  .pipe(gulp.dest('./dist/images'));
+}
+
+exports.default = gulp.parallel(styles, scripts, images);
 exports.watch = function() {
   gulp.watch('./src/styles/*.scss', gulp.parallel(styles));
   gulp.watch('./src/scripts/*.js', gulp.parallel(scripts));
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,30 @@
-console.log("Olá, mundo!")
+document.addEventListener('DOMContentLoaded', function() {
+  const buttons = document.querySelectorAll('[data-tab-button]');
+  
+  for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', function(btn) {
+      const tabTarget = btn.target.dataset.tabButton;
+      const tab = document.querySelector(`[data-tab-id=${tabTarget}]`);
+      hideAllTabs();
+      tab.classList.add('movies__list--is-active');
+      removeActiveButton();
+      btn.target.classList.add('movies__tabs__button--is-active');
+    })
+  }
+})
+
+function removeActiveButton() {
+  const buttons = document.querySelectorAll('[data-tab-button]');
+
+  for (let i = 0; i < buttons.length; i++) {
+    buttons[i].classList.remove('movies__tabs__button--is-active');
+  }
+}
+
+function hideAllTabs() {
+  const tabsContainer = document.querySelectorAll('[data-tab-id]');
+
+  for (let i = 0; i < tabsContainer.length; i++) {
+    tabsContainer[i].classList.remove('movies__list--is-active');
+  }
+}
